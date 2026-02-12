9ac5b6e6e45fb5a9c039da4ce4c12332aa6c86bf
Suok9 <suoephraim@gmail.com>
2026-02-12 17:30:56 +0100

    Update Recursion.cs

--- week05/code/Recursion.cs
+++ week05/code/Recursion.cs
@@ -106,6 +106,10 @@ public static class Recursion
     /// </summary>
     public static decimal CountWaysToClimb(int s, Dictionary<int, decimal>? remember = null)
     {
+// Initialize memo dictionary
+    if (remember == null)
+        remember = new Dictionary<int, decimal>();
+
         // Base Cases
         if (s == 0)
             return 0;
@@ -116,10 +120,15 @@ public static class Recursion
         if (s == 3)
             return 4;
 
-        // TODO Start Problem 3
+          if (remember.ContainsKey(s))
+        return remember[s];
 
         // Solve using recursion
         decimal ways = CountWaysToClimb(s - 1) + CountWaysToClimb(s - 2) + CountWaysToClimb(s - 3);
+
+ // Store result
+    remember[s] = ways;
+
         return ways;
     }
 
